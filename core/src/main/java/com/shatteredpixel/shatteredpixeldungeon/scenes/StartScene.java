5f5728e049f2da3087b322fb82c064a708d9a3d1
Hoto-Mocha <mtj2010@naver.com>
2025-03-23 01:50:33 +0900

    세이브 선택 화면에서 EX 스킬 이름이 출력되는 문제 수정

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/StartScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/StartScene.java
@@ -209,13 +209,15 @@ public class StartScene extends PixelScene {
 					level = null;
 				}
 			} else {
-				
-				if (info.subClass != HeroSubClass.NONE){
-					name.text(Messages.titleCase(info.subClass.title()));
-				} else {
-					name.text(Messages.titleCase(info.heroClass.title()));
-				}
-				
+
+//				if (info.subClass != HeroSubClass.NONE){
+//					name.text(Messages.titleCase(info.subClass.title()));
+//				} else {
+//					name.text(Messages.titleCase(info.heroClass.title()));
+//				}
+
+				name.text(Messages.titleCase(info.heroClass.title()));
+
 				if (hero == null){
 					hero = new Image(info.heroClass.spritesheet(), 0, 15*info.armorTier, 12, 15);
 					add(hero);
--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndGameInProgress.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndGameInProgress.java
@@ -25,7 +25,6 @@ import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.GamesInProgress;
 import com.shatteredpixel.shatteredpixeldungeon.ShatteredPixelDungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
-import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroSubClass;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.InterlevelScene;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.PixelScene;
