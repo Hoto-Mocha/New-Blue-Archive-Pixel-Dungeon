a45d18d0fa0ae8f708f06852fccaaad0cb6ef043
Hoto-Mocha <mtj2010@naver.com>
2025-04-03 12:44:40 +0900

    유니크 아이템으로 변경

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/gun/MG/MG_SP.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/gun/MG/MG_SP.java
@@ -8,10 +8,17 @@ public class MG_SP extends MG {
         image = ItemSpriteSheet.MG_SPECIAL;
 
         tier = 3;
+        bones = false;
+        unique = true;
     }
 
     @Override
     public int tier() {
         return super.tier() + ((Dungeon.hero != null) ? (int)(Dungeon.hero.lvl/20f) : 0);
+    }
+
+    @Override
+    public int value() {
+        return -1;
     }
 }
