6de5c86dc97df40485d061f5ef1dcd4b17a8423f
Hoto-Mocha <mtj2010@naver.com>
2025-04-06 01:51:12 +0900

    노노미 교복 능력 2 및 4-1, 4-2, 4-3 특성 구현

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/nonomi/BlackCard.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/nonomi/BlackCard.java
@@ -1,10 +1,18 @@
 package com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.nonomi;
 
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Invisibility;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Light;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Talent;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.ArmorAbility;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
+import com.shatteredpixel.shatteredpixeldungeon.effects.FloatingText;
+import com.shatteredpixel.shatteredpixeldungeon.items.Gold;
 import com.shatteredpixel.shatteredpixeldungeon.items.armor.ClassArmor;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
 import com.shatteredpixel.shatteredpixeldungeon.ui.HeroIcon;
 
 public class BlackCard extends ArmorAbility {
@@ -17,8 +25,25 @@ public class BlackCard extends ArmorAbility {
         armor.charge -= chargeUse(hero);
         armor.updateQuickslot();
         Invisibility.dispel();
+        float damagePerGold = 30f - 5f * hero.pointsInTalent(Talent.NONOMI_ARMOR2_1);
+        int damage = (int)(Dungeon.gold/damagePerGold);
+
+        if (hero.hasTalent(Talent.NONOMI_ARMOR2_3)) {
+            Buff.affect(hero, Light.class, Math.round(Dungeon.gold/(5f-hero.pointsInTalent(Talent.NONOMI_ARMOR2_3))));
+        }
+
+        hero.sprite.showStatusWithIcon( CharSprite.NEGATIVE, Integer.toString(-Dungeon.gold), FloatingText.GOLD );
+        Dungeon.gold = 0;
+
+        for (Mob mob : Actor.enemiesInHeroFOV(Dungeon.level)) {
+            mob.damage(damage, hero);
+            if (hero.hasTalent(Talent.NONOMI_ARMOR2_2) && !mob.isAlive()) {
+                new Gold().quantity(20*hero.pointsInTalent(Talent.NONOMI_ARMOR2_2)).doPickUp(hero);
+            }
+        }
 
         hero.sprite.operate(hero.pos);
+        hero.spendAndNext(1f);
     }
 
     @Override
