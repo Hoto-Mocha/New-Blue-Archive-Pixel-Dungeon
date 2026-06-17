7676d54d4fd36053a1c393508679da5400ece2e8
Mocha <mtj2010@naver.com>
2026-06-18 00:20:47 +0900

    이동 애니메이션 속도 조정

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/OtogiSprite.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/OtogiSprite.java
@@ -26,7 +26,7 @@ public class OtogiSprite extends MobSprite {
         idle = new Animation( 1, true );
         idle.frames( film, 0, 0, 0, 1, 0, 0, 1, 1 );
 
-        run = new Animation( 20, true );
+        run = new Animation( 16, true );
         run.frames( film, 2, 3, 4, 5, 6, 7 );
 
         die = new Animation( 20, false );
