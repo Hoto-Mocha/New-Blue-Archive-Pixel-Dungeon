ab769200e18a5af42b5f402e3cb449be1c5349b2
Mocha <mtj2010@naver.com>
2026-06-18 21:04:12 +0900

    시로코 갑옷 능력 2 특성 구현

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/shiroko/WeakeningGrenade.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/abilities/shiroko/WeakeningGrenade.java
@@ -3,19 +3,23 @@ package com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.shiroko;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Blindness;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Invisibility;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Paralysis;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Weakness;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Talent;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.ArmorAbility;
 import com.shatteredpixel.shatteredpixeldungeon.items.active.Grenade;
 import com.shatteredpixel.shatteredpixeldungeon.items.armor.ClassArmor;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.gun.Gun;
 import com.shatteredpixel.shatteredpixeldungeon.mechanics.Ballistica;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 import com.shatteredpixel.shatteredpixeldungeon.ui.HeroIcon;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
+import com.watabou.utils.Random;
 
 public class WeakeningGrenade extends ArmorAbility {
     {
@@ -49,6 +53,12 @@ public class WeakeningGrenade extends ArmorAbility {
 
         new WeakeningGrenadeItem().knockItem().cast(hero, target);
 
+        Gun gun = null;
+        if (hero.belongings.weapon() instanceof Gun) gun = (Gun) hero.belongings.weapon();
+        if (gun != null && hero.hasTalent(Talent.SHIROKO_ARMOR2_3)) {
+            gun.manualReload(hero.pointsInTalent(Talent.SHIROKO_ARMOR2_3), true);
+        }
+
         Invisibility.dispel();
         armor.charge -= chargeUse(hero);
         armor.updateQuickslot();
@@ -57,8 +67,28 @@ public class WeakeningGrenade extends ArmorAbility {
     public static class WeakeningGrenadeItem extends Grenade {
 
         @Override
+        public int explodeMinDmg(){
+            return Math.round((5 + Dungeon.scalingDepth()) * (1+0.25f*Dungeon.hero.pointsInTalent(Talent.SHIROKO_ARMOR2_1)));
+        }
+
+        @Override
+        public int explodeMaxDmg(){
+            return Math.round((10 + Dungeon.scalingDepth()*2) * (1+0.25f*Dungeon.hero.pointsInTalent(Talent.SHIROKO_ARMOR2_1)));
+        }
+
+        @Override
         public void effectsOnChar(Char ch) {
-            Buff.affect(ch, Weakness.class, 10f);
+            Buff.affect(ch, Weakness.class, Math.round(10f * (1+0.25f*Dungeon.hero.pointsInTalent(Talent.SHIROKO_ARMOR2_1))));
+
+            if (Dungeon.hero.hasTalent(Talent.SHIROKO_ARMOR2_2)) {
+                float chance = 0.3f*Dungeon.hero.pointsInTalent(Talent.SHIROKO_ARMOR2_2);
+                if (Random.Float() < chance) {
+                    Buff.affect(ch, Blindness.class, 5f);
+                }
+                if (Random.Float() < chance-1) {
+                    Buff.affect(ch, Paralysis.class, 5f);
+                }
+            }
         }
 
         @Override
@@ -81,12 +111,21 @@ public class WeakeningGrenade extends ArmorAbility {
             public int throwPos(Hero user, int dst) {
                 return new Ballistica( user.pos, dst, Ballistica.STOP_TARGET | Ballistica.STOP_SOLID ).collisionPos;
             }
+
+            @Override
+            public float castDelay(Char user, int cell) {
+                if (Random.Float() < 0.25f*Dungeon.hero.pointsInTalent(Talent.SHIROKO_ARMOR2_3)) {
+                    return 0;
+                } else {
+                    return super.castDelay(user, cell);
+                }
+            }
         }
     }
 
 
     @Override
     public Talent[] talents() {
-        return new Talent[]{Talent.SHIROKO_ARMOR1_1, Talent.SHIROKO_ARMOR1_2, Talent.SHIROKO_ARMOR1_3, Talent.HEROIC_ENERGY};
+        return new Talent[]{Talent.SHIROKO_ARMOR2_1, Talent.SHIROKO_ARMOR2_2, Talent.SHIROKO_ARMOR2_3, Talent.HEROIC_ENERGY};
     }
 }
