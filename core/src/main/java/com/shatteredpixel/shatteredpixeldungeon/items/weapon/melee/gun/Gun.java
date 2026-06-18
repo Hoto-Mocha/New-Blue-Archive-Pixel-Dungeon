79863627630d69015fd3bf1db2b6eb8080fb33fd
Mocha <mtj2010@naver.com>
2026-06-18 15:59:31 +0900

    Reapply "추가한 변수 및 메서드를 제거"
    This reverts commit 1e44d58d07b2871d7adad419a456b585b725b855.

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/gun/Gun.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/gun/Gun.java
@@ -720,8 +720,6 @@ public class Gun extends MeleeWeapon {
 
         private float accMulti = 1f;
         private boolean ignoreWall = false;
-        private boolean playSound = true;
-        private boolean usesTurn = true;
 
         public void setAccMulti(float multi) {
             accMulti = multi;
@@ -729,14 +727,6 @@ public class Gun extends MeleeWeapon {
 
         public void setIgnoreWall(boolean isIgnore) {
             ignoreWall = isIgnore;
-        }
-
-        public void setPlaySound(boolean doesPlay) {
-            playSound = doesPlay;
-        }
-
-        public void setUsesTurn(boolean hasDelay) {
-            usesTurn = hasDelay;
         }
 
         @Override
@@ -839,7 +829,7 @@ public class Gun extends MeleeWeapon {
 
         @Override
         public float castDelay(Char user, int cell) {
-            return usesTurn ? delayFactor(user) : 0;
+            return delayFactor(user);
         }
 
         @Override
@@ -998,9 +988,7 @@ public class Gun extends MeleeWeapon {
 
         @Override
         public void throwSound() {
-            if (playSound) {
-                Sample.INSTANCE.play( Assets.Sounds.HIT_CRUSH, 1, Random.Float(0.33f, 0.66f) );
-            }
+            Sample.INSTANCE.play( Assets.Sounds.HIT_CRUSH, 1, Random.Float(0.33f, 0.66f) );
         }
 
         @Override
