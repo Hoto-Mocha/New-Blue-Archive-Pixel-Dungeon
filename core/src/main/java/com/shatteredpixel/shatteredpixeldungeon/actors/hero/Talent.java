1bae0b0612f79d03a24d5257d8c3d4a1a04a8fab
Mocha <mtj2010@naver.com>
2026-07-10 20:29:20 +0900

    미유 2-1 특성의 성능을 조정

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Talent.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Talent.java
@@ -1340,7 +1340,7 @@ public enum Talent {
 			Buff.affect(hero, Healing.class).setHeal(2+3*hero.pointsInTalent(Talent.NOA_T2_1), 0, 1);
 		}
 		if (hero.hasTalent(Talent.MIYU_T2_1)) {
-			Buff.affect(hero, Invisibility.class, 2+3*hero.pointsInTalent(Talent.MIYU_T2_1));
+			Buff.affect(hero, Invisibility.class, 1+2*hero.pointsInTalent(Talent.MIYU_T2_1));
 		}
 		if (hero.hasTalent(Talent.YUZU_T1_1)) {
 			Buff.affect(hero, YuzuStatus.CertainCritBuff.class).countUp(1+hero.pointsInTalent(Talent.YUZU_T1_1));
