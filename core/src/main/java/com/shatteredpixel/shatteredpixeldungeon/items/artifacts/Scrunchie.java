02345895e7a39abee98cf7d565832d896db7593e
Mocha <mtj2010@naver.com>
2026-09-15 17:17:43 +0900

    헤어슈슈의 공격 피해 증가량을 감소

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/artifacts/Scrunchie.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/artifacts/Scrunchie.java
@@ -141,7 +141,7 @@ public class Scrunchie extends Artifact {
                         if (curUser.hasTalent(Talent.MIKA_T2_3)) {
                             throwPower += 1+curUser.pointsInTalent(Talent.MIKA_T2_3);
                         }
-                        curUser.attack(ch, 1.5f, 0, Char.INFINITE_ACCURACY);
+                        curUser.attack(ch, 1.2f, 0, Char.INFINITE_ACCURACY);
                         Elastic.pushEnemy(curUser, ch, null, throwPower);
                         curUser.spendAndNext(curUser.attackDelay());
                     }
