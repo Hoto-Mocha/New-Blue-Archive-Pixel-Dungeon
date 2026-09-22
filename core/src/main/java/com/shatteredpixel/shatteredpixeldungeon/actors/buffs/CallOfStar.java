f1ba0e87ea11b8c48b7744689080d0b27267213b
Mocha <mtj2010@naver.com>
2026-09-22 23:06:48 +0900

    미카 EX1: 최대 쿨타임을 200턴으로 제한

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/CallOfStar.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/CallOfStar.java
@@ -257,7 +257,7 @@ public class CallOfStar extends CounterBuff implements ActionIndicator.Action {
     private float cooldownTime(float count, int killCount, Hero hero) {
         float reductionRate = 1-0.05f*hero.pointsInTalent(Talent.MIKA_EX1_1)*killCount;
         reductionRate = Math.max(reductionRate, 0);
-        return count*2*reductionRate;
+        return Math.min(count*2*reductionRate, CallOfStarCooldown.DURATION);
     }
 
     public static boolean isWallBreakable(int depth, int cell) {
